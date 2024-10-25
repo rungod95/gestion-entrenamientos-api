5d6c732cb8a4edddffe700197f97be7b72968127
Jplanas <jabierplanos@gmail.com>
2024-10-25 23:36:47 +0200

    Primer commit: archivo readme

---
+++ src/test/java/com/example/Apitrain/ApitrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Apitrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApitrainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
